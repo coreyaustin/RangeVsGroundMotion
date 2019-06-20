4a42244a329476ed4912657ee9795d72ab7ed14d
Corey Austin <corey.austin@cr16.cds.ligo-la.caltech.edu>
2019-06-20 17:43:57 -0500

    added class to script

--- rangeVsMotion.py
+++ rangeVsMotion.py
@@ -9,41 +9,72 @@ Created on Wed Jun 19 18:46:46 2019
 from gwpy.timeseries import TimeSeriesDict
 from matplotlib import pyplot as plt
 import numpy as np
+from os import path
 
+class rangeData:
+    
+    default_start = 'June 16 2019 00:00:00 UTC'
+    default_end   = 'June 18 2019 23:00:00 UTC'
+    
+    def __init__(self,channels,filename,start=default_start,end=default_end):
+        self.channels = channels
+        if path.exists('./data/{}.hdf5'.format(filename)):
+            self.data = TimeSeriesDict.read('./data/{}.hdf5'.format(filename))
+        else:
+            self.data = TimeSeriesDict.fetch(channels,start,end)
+            self.data.write('./data/{}.hdf5'.format(filename))
+            
+    def cullGlitches(self):
+        range = self.data[self.channels[0]]
+        index = np.where(range.value>120)
+        for i in self.channels:
+            self.data[i] = self.data[i][index]
+        
+        
+    def averageRange(self,motion):
+        for i in self.channels[1:]:
+            self.data[i].avg_range = np.zeros(len(motion))
+            for j in xrange(len(motion)):
+                idx = np.where((self.data[i].value>motion[j]-25)&(self.data[i].value<motion[j]+25))
+                self.data[i].avg_range[j] = np.mean(self.data[self.channels[0]][idx].value)
+        
+        
+    
+#%%
 channels = ['L1:DMT-SNSH_EFFECTIVE_RANGE_MPC.mean',
             'L1:ISI-GND_STS_ETMY_Y_BLRMS_100M_300M.mean,m-trend',
             'L1:ISI-GND_STS_ETMY_Y_BLRMS_1_3.mean,m-trend',
-            'L1:ISI-GND_STS_ETMY_Y_BLRMS_3_10.mean,m-trend']
+            'L1:ISI-GND_STS_ETMY_Y_BLRMS_3_10.mean,m-trend',
+            'L1:ISI-GND_STS_ETMX_X_BLRMS_100M_300M.mean,m-trend',
+            'L1:ISI-GND_STS_ETMX_X_BLRMS_1_3.mean,m-trend',
+            'L1:ISI-GND_STS_ETMX_X_BLRMS_3_10.mean,m-trend',
+            'L1:ISI-GND_STS_ITMY_Z_BLRMS_100M_300M.mean,m-trend',
+            'L1:ISI-GND_STS_ITMY_Z_BLRMS_1_3.mean,m-trend',
+            'L1:ISI-GND_STS_ITMY_Z_BLRMS_3_10.mean,m-trend']
 
-start = 'June 16 2019 00:00:00 UTC'
+start = 'June 11 2019 00:00:00 UTC'
 end   = 'June 18 2019 23:00:00 UTC'
 
-data = TimeSeriesDict.fetch(channels,start,end)
-data.wri
-range = data[channels[0]]
+filename = '190611_to_190618_data'
 
-#%%
-idx = np.where(range.value>120)
-range  = range[idx]
-useism = data[channels[1]][idx]
-anthro = data[channels[2]][idx]
-#%%
-motion = np.arange(50,510,10)
-avg_range = np.zeros(len(motion))
-for i in xrange(len(motion)):
-    index = np.where((anthro.value>motion[i]-25)&(anthro.value<motion[i]+25))
-    avg_range[i] = np.mean(range[index].value)
-    
-#motion = np.arange(50,510,10)
-#avg_range = np.zeros(len(motion))
-#for i in xrange(len(motion)):
-#    index = np.where((useism.value>motion[i]-25)&(useism.value<motion[i]+25))
-#    avg_range[i] = np.mean(range[index].value)
+motion = np.arange(50,1000,10)
 
-for i in channels[1:]:
+noise = rangeData(channels,filename,start=start,end=end)
+noise.cullGlitches()
+noise.averageRange(motion)
     
 #%%
-plt.scatter(motion,avg_range)
-    
+plt.rcParams.update({'text.usetex': False})
+fig, axes = plt.subplots(3,3,sharex=True,sharey=True,figsize=[16*1.5,9*1.5])
+
+for i in range(3):
+    for j in range(3):
+        index = (i * 3) + j + 1
+        axes[i,j].scatter(motion,noise.data[channels[index]].avg_range)
+        axes[i,j].set_title(channels[index][15:-13],fontsize=16)
+axes[0,0].set_ylim([125,140])
+for ax in axes.flat:
+    ax.set(xlabel='Ground Motion (nm/s)',ylabel='Range (Mpc)')    
+    ax.label_outer()
 
 
